aad2a3d5a716539b0ff1bf8a78e3971c27410988
Anton Ponadiozin <anton.ponadiozin@adv.bet>
2022-12-28 07:26:39 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const github = require('@actions/github');
+const core = require('@actions/core');
+
+const run = async () => {
+  const token = core.getInput('github-token', { required: false }) || process.env.GITHUB_TOKEN;
+
+  if (!token) {
+    throw new Error('No GITHUB_TOKEN found');
+  }
+
+  const octokit = github.getOctokit(token);
+  const context = github.context;
+
+  const reviews = await octokit.pulls.listReviews({
+    ...context.repo,
+    pull_number: context.payload.pull_request.number
+  });
+
+  core.info(reviews);
+};
+
+run()
+  .then(() => {
+    core.info('Done.');
+  })
+  .catch((e) => {
+    core.error(e.message);
+  });
